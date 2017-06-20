55d6c8096382ea0ab989a05e4fcfd2c0e93f4a5d
zhubinchen <cheng4741@qq.com>
2017-06-20 16:55:06 +0800

    refactor: rewrite with swift 3.0

---
+++ MarkLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarkLite
+//
+//  Created by zhubch on 2017/6/20.
+//  Copyright © 2017年 zhubch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MarkLite' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'EZSwiftExtensions'
+
+  # Pods for MarkLite
+
+end
