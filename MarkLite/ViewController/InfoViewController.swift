8fea2cd777a961383ec8df66ff9d84257e26c80b
BingCheng <cheng4741@qq.com>
2019-07-19 11:50:18 +0800

    feat: purchase

---
+++ MarkLite/ViewController/InfoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  InfoViewController.swift
+//  Markdown
+//
+//  Created by 朱炳程 on 2019/7/19.
+//  Copyright © 2019 zhubch. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InfoViewController: UIViewController, WKNavigationDelegate {
+    
+    let webView = WKWebView(frame: CGRect())
+    
+    var urlString = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addSubview(webView)
+        
+        guard let url = URL(string: urlString) else { return }
+        self.webView.load(URLRequest(url: url))
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action:#selector(close))
+        
+        self.view.startLoadingAnimation()
+    }
+    
+    @objc func close() {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = self.view.bounds;
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.view.stopLoadingAnimation()
+    }
+}
--- MarkLite/ViewController/PurchaseViewController.swift
+++ MarkLite/ViewController/PurchaseViewController.swift
@@ -14,24 +14,76 @@ class PurchaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = /"Premium"
-
     }
     
 
     @IBAction func subscribe(_ sender: UIButton!) {
-        
+        purchaseProduct(premiumProductID)
     }
     
     @IBAction func restore(_ sender: UIButton!) {
+        self.view.startLoadingAnimation()
         
+        IAP.restorePurchases { (identifiers, error) in
+            if let err = error {
+                print(err.localizedDescription)
+                self.showAlert(title: /"RestoreFailed")
+                self.view.stopLoadingAnimation()
+                return
+            }
+//            Configure.shared.checkVipAvailable({ (availabel) in
+//                if availabel {
+//                    self.showAlert(title: /"RestoreSuccess")
+//                    self.dismiss(nil)
+//                } else {
+//                    self.showAlert(title: /"RestoreFailed")
+//                }
+//                self.view.stopLoadingAnimation()
+//            })
+            print(identifiers)
+        }
     }
 
     @IBAction func privacy(_ sender: UIButton!) {
-        
+        let vc = InfoViewController()
+        vc.urlString = "https://www.freeprivacypolicy.com/privacy/view/0c071aa1336dd9e73585f2535466bcf6"
+        vc.title = /"Privacy"
+        let nav = UINavigationController(rootViewController: vc)
+        presentVC(nav)
     }
 
     @IBAction func terms(_ sender: UIButton!) {
-        
+        let vc = InfoViewController()
+        vc.urlString = "https://www.freeprivacypolicy.com/privacy/view/0c071aa1336dd9e73585f2535466bcf6"
+        vc.title = /"Terms"
+        let nav = UINavigationController(rootViewController: vc)
+        presentVC(nav)
+    }
+    
+    func purchaseProduct(_ identifier: String) {
+        self.view.startLoadingAnimation()
+        IAP.requestProducts([identifier]) { (response, error) in
+            guard let product = response?.products.first else {
+                self.view.stopLoadingAnimation()
+                return
+            }
+            IAP.purchaseProduct(product.productIdentifier, handler: { (identifier, error) in
+                if error != nil {
+                    self.view.stopLoadingAnimation()
+                    print(error?.localizedDescription ?? "")
+                    return
+                }
+//                Configure.shared.checkVipAvailable({ (availabel) in
+//                    if availabel {
+//                        self.showAlert(title: /"SubscribedSuccess")
+//                        self.dismiss(nil)
+//                    } else {
+//                        self.showAlert(title: /"SubscribeFailed")
+//                    }
+//                    self.view.stopLoadingAnimation()
+//                })
+            })
+        }
     }
 
 }
